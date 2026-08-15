e483638b99e80ee29eab07158c1a3ac242cc4168
Alyssa <alyssaphelps3@gmail.com>
2026-08-15 23:06:08 +0100

    Fixed item conunting bug and and make code more robust using a dictionary of the products and looping over the keys

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function updateCartTable(cart){
 
 // Uses functions to adjust elements based on user interaction
 class ItemCounting {
-    constructor (itemName, quantityField, itemQuantity, itemPrice) {
+    constructor (itemName, quantityField, itemPrice) {
         this.itemName = itemName
         this.quantityField = quantityField
-        this.itemQuantity = itemQuantity
+        this.itemQuantity = 0
         this.itemPrice = itemPrice
     }
     updatingFields() {
@@ -39,292 +39,52 @@ class ItemCounting {
         this.itemQuantity ++;
         totalItemCount ++;
         this.quantityField.textContent = ` ${this.itemQuantity} `;
-        totalpay = totalpay + this.itemPrice};
+        cart[this.itemName] = this.itemQuantity;
+        totalpay = totalpay + this.itemPrice;
+        this.updatingFields()
+        updateCartTable()
+    };
     deleteItem(){
         if (this.itemQuantity > 0){
             totalItemCount --;
             this.itemQuantity--;
-            totalpay = totalpay - this.itemPrice
-        };
-        icecreamquantity.textContent=`${totalItemCount} Items £${totalpay.toFixed(2)}`;
-    }};
+            totalpay = totalpay - this.itemPrice;
+            if (this.itemQuantity === 0){delete cart [this.itemName]};
+            this.updatingFields()
+            updateCartTable()
+        }
+    }
+}
 
-// Latte Functionality
-let lattequantity = 0;
-let latte = new ItemCounting("Latte", document.getElementById("lattecount"), lattequantity, 2.39);
+// Object of items sold
+const menuItems = {
+    latte: new ItemCounting("Latte", document.getElementById("lattecount"), 2.39),
+    americano: new ItemCounting("Americano", document.getElementById("americanocount"), 1.79),
+    flatwhite: new ItemCounting("Flat White", document.getElementById("flatwhitecount"), 1.64),
+    cappucino: new ItemCounting("Cappuccino", document.getElementById("cappuccinocount"), 3.09),
+    cookies: new ItemCounting("Chocolate Chip Cookie", document.getElementById("cookiescount"), 0.78),
+    brownies: new ItemCounting("Brownie", document.getElementById("browniescount"), 1.05),
+    donut: new ItemCounting("Sprinkled Donut", document.getElementById("donutcount"), 0.75), 
+    icecream: new ItemCounting("Dairy Milk Ice Cream", document.getElementById("icecreamcount"), 1.68),
+    chocomilk: new ItemCounting("Milk Chocolate Milkshake", document.getElementById("chocomilkcount"), 1.99),
+    oreoMilkshake: new ItemCounting("Oreo Milkshake", document.getElementById("oreomilkcount"), 2.10),
+    strawmilk: new ItemCounting("Strawberry Milkshake", document.getElementById("strawmilkcount"), 1.89),
+    belgian: new ItemCounting("Belgian Chocolate Milkshake", document.getElementById("belgiancount"), 1.68),
+    punch: new ItemCounting("Fruit Punch Smoothie", document.getElementById("punchcount"), 3.04),
+    pine: new ItemCounting("Pineapple Smoothie", document.getElementById("pinecount"), 2.98),
+    mango: new ItemCounting("Apple and Mango Smoothie", document.getElementById("mangocount"), 2.68),
+    drag: new ItemCounting("Dragon Fruit and Raspberry Smoothie", document.getElementById("dragcount"), 2.38),
+}
 
-document.getElementById("morelatte").onclick = function(){
-    cart["Latte"] = lattequantity
-    latte.addItem();
-    latte.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lesslatte").onclick = function(){
-    latte.deleteItem();
-    if (lattequantity=== 0){delete cart ["Latte"]};
-    latte.updatingFields();
-    updateCartTable(cart);
-};
+Object.keys(menuItems).forEach(key => {
+    // Declaring Variables holding buttons 
+    const moreButton = document.getElementById(`more${key}`);
+    const lessButton = document.getElementById(`less${key}`);
+    // Creating the functionality for the 2 declared buttons
+    moreButton.onclick= function(){menuItems[key].addItem()}
+    lessButton.onclick= function(){menuItems[key].deleteItem()}
+})
 
-// Americano Functionality
-let americanoquantity  = 0;
-let americano = new ItemCounting("Americano", document.getElementById("americanocount"), americanoquantity, 1.79);
-
-document.getElementById("moreamericano").onclick = function(){
-    cart["Americano"] = americanoquantity
-    americano.addItem();
-    americano.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessamericano").onclick = function(){
-    if (americanoquantity === 0){delete cart ["Americano"]};
-    americano.deleteItem();
-    americano.updatingFields();
-    updateCartTable(cart);
-};
-
-// Flat White Functionality
-let flatwhitequantity = 0 ;
-let flatWhite = new ItemCounting("Flat White", document.getElementById("flatwhitecount"), flatwhitequantity, 1.64);
-
-document.getElementById("moreflatwhite").onclick = function(){
-    cart["Flat White"] = flatwhitequantity;
-    flatWhite.addItem();
-    flatWhite.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessflatwhite").onclick = function(){
-    if (flatwhitequantity === 0){delete cart ["Flat White"]};
-    flatWhite.deleteItem();
-    flatWhite.updatingFields();
-    updateCartTable(cart);
-};
-
-// Cappucino Functionality
-let cappucinoquantity = 0;
-let cappucino = new ItemCounting("Cappuccino", document.getElementById("cappuccinocount"), cappucinoquantity, 3.09);
-
-document.getElementById("morecappuccino").onclick = function(){
-    cart["Cappuccino"] = cappucinoquantity;
-    cappucino.addItem();
-    cappucino.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lesscappuccino").onclick = function(){
-    if (cappucinoquantity === 0){delete cart ["Cappuccino"]};
-    cappucino.deleteItem();
-    cappucino.updatingFields();
-    updateCartTable(cart);
-};
-
-// Cookies Functionality
-let cookiesquantity = 0;
-let cookies = new ItemCounting("Chocolate Chip Cookie", document.getElementById("cookiescount"), cookiesquantity, 0.78);
-
-document.getElementById("morecookies").onclick = function(){
-    cart["Chocolate Chip Cookie"] = cookiesquantity;
-    cookies.addItem();
-    cookies.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lesscookies").onclick = function(){
-    cart["Chocolate Chip Cookie"] = cookiesquantity;
-    if (cookiesquantity === 0){delete cart ["Chocolate Chip Cookie"]}
-    cookies.deleteItem();
-    cookies.updatingFields();
-    updateCartTable(cart);
-};
-
-// Brownies Functionality
-let browniesquantity = 0;
-let brownies = new ItemCounting("Brownie", document.getElementById("browniescount"), browniesquantity, 1.05);
-
-document.getElementById("morebrownies").onclick = function(){
-    cart["Brownie"] = browniesquantity;
-    brownies.addItem();
-    brownies.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessbrownies").onclick = function(){
-    if (browniesquantity === 0){delete cart ["Brownie"]};
-    brownies.deleteItem();
-    brownies.updatingFields();
-    updateCartTable(cart);
-};
-
-// Donut Functionality
-let donutquantity = 0;
-let donut = new ItemCounting("Sprinkled Donut", document.getElementById("donutcount"), donutquantity, 0.75);
-
-document.getElementById("moredonut").onclick = function(){
-    cart["Sprinkled Donut"] = donutquantity;
-    donut.addItem();
-    donut.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessdonut").onclick = function(){
-    if (donutquantity === 0){delete cart ["Sprinkled Donut"]};
-    donut.deleteItem();
-    donut.updatingFields();
-    updateCartTable(cart);
-};
-
-// Ice Cream Functionality
-let icecreamquantity = 0;
-let icecream = new ItemCounting("Dairy Milk Ice Cream", document.getElementById("icecreamcount"), icecreamquantity, 1.68);
-
-document.getElementById("moreicecream").onclick = function(){
-    cart["Dairy Milk Ice Cream"] = icecreamquantity;
-    icecream.addItem();
-    icecream.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessicecream").onclick = function(){
-    if (icecreamquantity === 0){delete cart ["Dairy Milk Ice Cream"]};
-    icecream.deleteItem();
-    icecream.updatingFields();
-    updateCartTable(cart);
-};
-
-// Chocolate Milkshake Functionality
-let chocomilkquantity = 0;
-let chocolateMilkshake = new ItemCounting("Milk Chocolate Milkshake", document.getElementById("chocomilkcount"), chocomilkquantity, 1.99);
-
-document.getElementById("morechocomilk").onclick = function(){
-   cart["Milk Chocolate Milkshake"] = chocomilkquantity;
-   chocolateMilkshake.addItem();
-   chocolateMilkshake.updatingFields();
-   updateCartTable(cart);
-};
-
-document.getElementById("lesschocomilk").onclick = function(){
-    if (chocomilkquantity === 0){delete cart ["Milk Chocolate Milkshake"]};
-    chocolateMilkshake.deleteItem();
-    chocolateMilkshake.updatingFields();
-    updateCartTable(cart);
-};
-
-// Oreo Milkshake Functionality
-let oreomilkquantity = 0;
-let oreoMilkshake = new ItemCounting("Oreo Milkshake", document.getElementById("oreomilkcount"), oreomilkquantity, 2.10);
-
-document.getElementById("moreoreomilk").onclick = function(){
-   cart["Oreo Milkshake"] = oreomilkquantity;
-   oreoMilkshake.addItem();
-   oreoMilkshake.updatingFields();
-   updateCartTable(cart);
-};
-document.getElementById("lessoreomilk").onclick = function(){
-    if (oreomilkquantity === 0){delete cart ["Oreo Milkshake"]};
-    oreoMilkshake.deleteItem();
-    oreoMilkshake.updatingFields();
-    updateCartTable(cart);
-};
-
-
-// Strawberry Milkshake Functionality
-let strawmilkquantity = 0;
-let strawberryMilkshake = new ItemCounting("Strawberry Milkshake", document.getElementById("strawmilkcount"), strawmilkquantity, 1.89);
-
-document.getElementById("morestrawmilk").onclick = function(){
-   cart["Strawberry Milkshake"] = strawmilkquantity;
-   strawberryMilkshake.addItem();
-   strawberryMilkshake.updatingFields();
-   updateCartTable(cart);
-};
-document.getElementById("lessstrawmilk").onclick = function(){
-    if (strawmilkquantity === 0){delete cart ["Strawberry Milkshake"]};
-    strawberryMilkshake.deleteItem();
-    strawberryMilkshake.updatingFields();
-    updateCartTable(cart);
-};
-
-// Belgian Chocolate Milkshake Functionality
-let belgianquantity = 0;
-let belgianChocolateMilkshake = new ItemCounting("Belgian Chocolate Milkshake", document.getElementById("belgiancount"), belgianquantity, 1.68);
-
-document.getElementById("morebelgian").onclick = function(){
-    cart["Belgian Chocolate Milkshake"] = belgianquantity;
-    belgianChocolateMilkshake.addItem();
-    belgianChocolateMilkshake.updatingFields();
-    updateCartTable(cart);
-};
-
-document.getElementById("lessbelgian").onclick = function(){
-    if (belgianquantity === 0){delete cart ["Belgian Chocolate Milkshake"]};
-    belgianChocolateMilkshake.deleteItem();
-    belgianChocolateMilkshake.updatingFields();
-    updateCartTable(cart)
-};
-
-// Fruit Punch Smoothie Functionality
-let punchquantity = 0;
-let fruitPunchSmoothie = new ItemCounting("Fruit Punch Smoothie", document.getElementById("punchcount"), punchquantity, 3.04);
-
-document.getElementById("morepunch").onclick = function(){
-    cart["Fruit Punch Smoothie"] = punchquantity;
-    fruitPunchSmoothie.addItem();
-    fruitPunchSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lesspunch").onclick = function(){
-    if (punchquantity === 0){delete cart ["Fruit Punch Smoothie"]};
-    fruitPunchSmoothie.deleteItem();
-    fruitPunchSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-
-// Pineapple Smoothie Functionality
-let pinequantity = 0;
-let pineappleSmoothie = new ItemCounting("Pineapple Smoothie", document.getElementById("pinecount"), pinequantity, 2.98);
-
-document.getElementById("morepine").onclick = function(){
-    cart["Pineapple Smoothie"] = pinequantity;
-    pineappleSmoothie.addItem();
-    pineappleSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lesspine").onclick = function(){
-    if (pinequantity === 0){delete cart ["Pineapple Smoothie"]};
-    pineappleSmoothie.deleteItem();
-    pineappleSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-
-// Apple and Mango Smoothie Functionality
-let mangoquantity = 0;
-let AppleMangoSmoothie = new ItemCounting("Apple and Mango Smoothie", document.getElementById("mangocount"), mangoquantity, 2.68);
-
-document.getElementById("moremango").onclick = function(){
-    cart["Apple and Mango Smoothie"] = mangoquantity;
-    AppleMangoSmoothie.addItem();
-    AppleMangoSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessmango").onclick = function(){
-    if (mangoquantity === 0){delete cart ["Apple and Mango Smoothie"]};
-    AppleMangoSmoothie.deleteItem();
-    AppleMangoSmoothie.updatingFields();
-    updateCartTable(cart);
-};
-
-// Dragon Fruit and Raspberry Smoothie Functionality
-let dragquantity = 0;
-let dragonFruitRaspberrySmoothie = new ItemCounting("Dragon Fruit and Raspberry Smoothie", document.getElementById("dragcount"), dragquantity, 2.38);
-
-document.getElementById("moredrag").onclick = function(){
-    cart["Dragon Fruit and Raspberry Smoothie"] = dragquantity;
-    dragonFruitRaspberrySmoothie.addItem();
-    dragonFruitRaspberrySmoothie.updatingFields();
-    updateCartTable(cart);
-};
-document.getElementById("lessdrag").onclick = function(){
-    if (dragquantity === 0){delete cart ["Dragon Fruit and Raspberry Smoothie"]};
-    dragonFruitRaspberrySmoothie.deleteItem();
-    dragonFruitRaspberrySmoothie.updatingFields();
-    updateCartTable(cart)
-};
- 
 // View Cart Table
 document.getElementById("pay-button").onclick= function () {
     document.getElementById("checkout-box").style.display = "block";
@@ -382,4 +142,4 @@ document.getElementById("paycheckout").onclick = function() {
         document.getElementById("itemcart").style.display = "none";
         document.getElementById("itemcount").textContent = "0 Items £0.00";
         document.getElementById("checkoutprice").textContent= "0 Items £0.00";
-    }};
+    }}
